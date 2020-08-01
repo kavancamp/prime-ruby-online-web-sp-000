6455b84504a970c7f79c4a3b462ac3bf56da5e11
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-01 16:48:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 # Add  code here!
-def prime?(num)
-  return if num <= 1
-  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
+def prime?(n)
+  return true if n == 2
+  return false if n % 2 == 0 || n < 2
+  (3..(n**0.5).floor).step(2) { |i|
+    return false if n % i == 0
+  }
+  true
 end
